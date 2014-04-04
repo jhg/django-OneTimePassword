bd65e8cc6bdcd3a6a4981255bd1094879c20232a
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2014-04-04 05:19:57 +0100

    Add setup.py for packaging django-OneTimePassword

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-OneTimePassword',
+    version='0.0.0',
+    packages=['django_OneTimePassword'],
+    include_package_data=True,
+    license='LGPL License',
+    description='A simple Django app with authentication backend for OneTimePassword.',
+    long_description=README,
+    url='https://github.com/jhg/django-OneTimePassword',
+    author='Jesus Hernandez Gormaz',
+    author_email='jhg.jesus@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: LGPL License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
