d968a4245d57dda067b0b08a3e7b3e32dfa2be4c
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2014-04-05 04:28:16 +0100

    Comment test

--- django_OneTimePassword/tests.py
+++ django_OneTimePassword/tests.py
@@ -1,20 +1,15 @@
 from django.test import TestCase
-from django_OneTimePassword.gen_password import sha512TS
+#from django.contrib.auth import get_user_model
+#from django_OneTimePassword.conf import OTP_TIME_SYNCHRONIZED
 
 
-#class gen_password_test(TestCase):
+#class OneTimePasswordTestCase(TestCase):
+#    def setUp(self):
+#        user_model = get_user_model()
+#        kwargs = {user_model.USERNAME_FIELD: "admin"}
+#        user_model.objects.create(**kwargs)
+#
 #    def test_sha512TS_gen_user_seed(self):
 #        """
 #        Tests seed generate by sha512TS.
 #        """
-#        instance = sha512TS()
-#        seed = instance.get_user_seed(
-#            "admin",
-#            length=6,
-#            interval=30,
-#            dictionary="abcdefghijklmnopqrstuvwxyz0123456789"
-#        )
-#        self.assertEqual(
-#            seed,
-#            ADMIN_SEED
-#        )
