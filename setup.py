7f65ec10e53db6e87b99e5a163c6a5ac7dfdcfb6
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2014-04-05 05:17:40 +0100

    Changes in setup.py and add files AUTHORS and HACKING

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-OneTimePassword',
-    version='0.0.0',
+    name='Django-OneTimePassword',
+    version='0.0.1',
     packages=['django_OneTimePassword'],
     include_package_data=True,
     license='LGPL License',
-    description='A simple Django app with authentication backend for OneTimePassword.',
+    description='A simple Django app with authentication backend for OneTimePassword',
     long_description=README,
     url='https://github.com/jhg/django-OneTimePassword',
     author='Jesus Hernandez Gormaz',
