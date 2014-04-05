fc929a4b70f1579bea364d7b4be50cc0c6af3234
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2014-04-05 05:57:02 +0100

    Fixed bugs with Pypi

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='Django-OneTimePassword',
-    version='0.0.1',
+    name='django-OneTimePassword',
+    version='0.0.2',
     packages=['django_OneTimePassword'],
     include_package_data=True,
     license='LGPL License',
